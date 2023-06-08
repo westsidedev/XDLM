733f5132720419c672f337a9a90c45bc4e34a562
WestSideDev <herdersonriker@gmail.com>
2023-06-08 11:51:05 -0300

    build 1.1(Colors)

---
+++ src/main/java/com/xdlmanager/xd/Colors.java
@@ -0,0 +1,7 @@
+public class Colors {
+	public String red = "\033[01;31m";
+	public String vd = "\033[01;32m";
+	public String yell = "\033[01;33m";
+	public String Cian = "\033[01;36m";
+	public String Close = "\033[m";
+}
--- src/main/java/com/xdlmanager/xd/Menu.java
+++ src/main/java/com/xdlmanager/xd/Menu.java
@@ -1,6 +1,9 @@
-//import com.xdlmanager.xd.Download;
 public class Menu{
 	public static void main(String[] args){
+		String red;
+		Colors colors = new Colors();
+		red = colors.red;
+		System.out.printf("%shello world",red);
 		Download dl = new Download();
 		dl.main();
 	}
