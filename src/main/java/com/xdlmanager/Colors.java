b5b4863034891fbbdb6ac35e6dc639c489c0c007
WestSideDev <herdersonriker@gmail.com>
2024-02-16 21:11:26 -0300

    refactor: Eliminando codigo desnecessario

--- src/main/java/com/xdlmanager/Colors.java
+++
@@ -1,9 +0,0 @@
-package com.xdlmanager;
-public class Colors {
-	public String blue = "\033[01;34m";
-	public String red = "\033[01;31m";
-	public String vd = "\033[01;32m";
-	public String yell = "\033[01;33m";
-	public String Cian = "\033[01;36m";
-	public String Close = "\033[m";
-}
---
+++ src/main/java/com/xdlmanager/utils/Colors.java
@@ -0,0 +1,10 @@
+package com.xdlmanager.utils;
+
+public class Colors {
+	String blue = "\033[01;34m";
+	String vermelho = "\033[01;31m";
+	String verde = "\033[01;32m";
+	String amarelo = "\033[01;33m";
+	String ciano = "\033[01;36m";
+	String semCor = "\033[m";
+}
